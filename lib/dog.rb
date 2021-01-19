4d8e02360bd70af13c5dde1daaac4e7e1aa7c347
Amedeo d'Amore <amedeodamore@gmail.com>
2021-01-19 10:33:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-def initialize(name, breed)
+def initialize(name, breed = "Mutt")
   @name = name
   @breed = breed
 end
@@ -13,7 +13,7 @@ def name
 end
 
 def breed=(breed)
-  @breed = Mutt
+  @breed = breed
 end
 
 def breed
